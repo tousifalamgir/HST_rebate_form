7235aa62bf332b063ddceb9e76718fceb089fdbe
tousifalamgir <tousifalamgir@gmail.com>
2024-04-09 07:35:12 -0400

    Got simplest form working

--- lib/form.dart
+++ lib/form.dart
@@ -8,18 +8,31 @@ class HstForm extends StatefulWidget {
 }
 
 class _HstFormState extends State<HstForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
-        children: [
-          Row(
-            children: [
-
-            ],
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Processing Data')),
+                );
+              }
+            },
+            child: const Text('submit'),
           )
         ],
         ),
